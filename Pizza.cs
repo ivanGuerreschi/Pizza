984a16dd048ee32241b2c8ea800b0ca114aacf84
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2021-11-06 16:29:21 +0100

    Add class Pizza.cs

---
+++ Pizza.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Pizza
+{
+    class Pizza
+    {
+	String TipoFarina { get; set; }
+	int GrammiFarina { get; set; }
+	String TipoLievito { get; set; }
+	int GrammiLievito { get; set; }
+	int GrammiAcqua { get; set; }
+	int GrammiSale { get; set; }
+	int GrammiOlio { get; set; }
+	int GrammiZucchero  { get; set; }
+	String Altro { get; set; }
+    }
+}
